b01d29ca68cbb9f528799221ca8363988bce0b73
Novi Indra Fanani <novi.indra1453@gmail.com>
2024-03-24 10:19:51 +0700

    add ssl pinning and firebase crashlitycs

---
+++ search/lib/search.dart
@@ -0,0 +1,10 @@
+library search;
+
+export 'movie/domain/usecases/search_movies.dart';
+export 'movie/presentation/provider/movie_search_notifier.dart';
+export 'movie/presentation/bloc/search_bloc.dart';
+export 'movie/presentation/pages/search_page.dart';
+
+export 'tv_series/domain/usecase/search_tv_series.dart';
+export 'tv_series/presentation/provider/tv_series_search_notifier.dart';
+export 'tv_series/presentation/pages/search_tv_series_page.dart';
